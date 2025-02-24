e385e5715f65303fa38edbd3b8b634d3bb7347bc
CommanderCero <dominikjeurissen@web.de>
2025-02-24 18:06:57 +0000

    Add torchRL test notebooks

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='odyssey',
+    version='0.1',
+    author='Dominik Jeurissen',
+    author_email='dominikjeurissen@web.de',
+    description='A doomed repo to get RL working with NetHack.',
+    packages=find_packages(),
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
